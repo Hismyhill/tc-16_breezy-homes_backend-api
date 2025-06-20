fe248b2d6cb974aa220749a259d9b63fb73a20c5
Ismaeel Owolabi <105186784+Hismyhill@users.noreply.github.com>
2025-06-20 11:15:08 +0100

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -3,12 +3,16 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DATABSE_URL,
-  {
-    dialect: process.env.DB_DIALECT || "mysql",
-    logging: Boolean(process.env.DB_LOGGING),
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: process.env.DB_DIALECT || "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Disable SSL validation
+    },
+  },
+  logging: Boolean(process.env.DB_LOGGING),
+});
+
 
 export default sequelize;
