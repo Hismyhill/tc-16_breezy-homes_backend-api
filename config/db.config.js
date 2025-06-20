f242133253f7b246e657ada17b2ef3e41d2a03d4
Ismaeel Owolabi <105186784+Hismyhill@users.noreply.github.com>
2025-06-20 01:58:54 +0100

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -4,11 +4,8 @@ import { Sequelize } from "sequelize";
 dotenv.config();
 
 const sequelize = new Sequelize(
-
   process.env.DB_URL,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || "mysql",
     logging: Boolean(process.env.DB_LOGGING),
   }
